2f56d278acc03c9eabdb815a305db702e72ad5af
kasai2007 <gurusainath138@gmail.com>
2026-06-27 07:53:01 +0530

    Create result.py

---
+++ result.py
@@ -0,0 +1,11 @@
+name = input("Enter student name: ")
+m1 = int(input("Enter marks 1: "))
+m2 = int(input("Enter marks 2: "))
+m3 = int(input("Enter marks 3: "))
+
+total = m1 + m2 + m3
+average = total / 3
+
+print("Name:", name)
+print("Total:", total)
+print("Average:", average)
