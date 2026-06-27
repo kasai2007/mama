37d348de1822a7272f5e57cdc688241211179bfc
kasai2007 <gurusainath138@gmail.com>
2026-06-27 07:53:34 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,11 @@
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 40:
+    grade = "D"
+else:
+    grade = "F"
+print("Grade:", grade)
